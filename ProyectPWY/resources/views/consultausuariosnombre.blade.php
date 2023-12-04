65237c6c89fa23a47dd8fe5b869bcce14e55e8d1
YaritziLeon <yaritzileonm08@gmail.com>
2023-12-04 01:17:09 -0600

    Controladores

---
+++ ProyectPWY/resources/views/consultausuariosnombre.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.plantilla')
+
+@section('titulo', 'Consulta de Usuario')
+
+@section('Contenido')
+<div class="container">
+    <br>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header bg-primary text-white">
+                    <h1 class="text-center">Consulta de Usuario</h1>
+                </div>
+                <div class="card-body">
+                    <form class="d-flex mb-5" role="search">
+                        <input class="form-control me-5" type="search" placeholder="Buscar Usuario" aria-label="search">
+                        <button class="btn btn-outline-success" type="submit">Buscar </button>
+                    </form>
+                    <div class="card-body">
+                        <table class="table table-striped-columns">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Apellido P</th>
+                                    <th scope="col">Apellido M</th>
+                                    <th scope="col">Correo</th>
+                                    <th scope="col">Puesto</th>
+                                    <th scope="col">Contraseña</th>
+                                </tr>
+                              </thead>
+                          </table>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+
+@endsection
